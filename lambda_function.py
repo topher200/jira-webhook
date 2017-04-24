2e70af4a3d2e498311d1320f0a741632b3322b0c
Topher Brown <topher200@gmail.com>
2017-04-24 01:16:55 -0400

    clean up logging

--- lambda_function.py
+++ lambda_function.py
@@ -13,16 +13,20 @@ API_REQUEST_URL = API_HOST_URL + API_ENDPOINT
 
 
 def lambda_handler(event, _):
-    print("Received event: " + json.dumps(event, indent=2))
+    # print("Received event: " + json.dumps(event, indent=2))
 
     key = json.loads(event['body'])['issue']['key']
     payload = {
         'issue_key': key
     }
+    print('making request: %s' % payload)
     res = requests.put(API_REQUEST_URL, json=payload)
     if res.status_code == 200:
         print('Successfully updated "%s"' % key)
     else:
-        print('request received %s. data: "%s"' % (res.status_code, payload))
+        print(
+            'request failed. received response: %s. our payload: "%s"' %
+            (res.status_code, payload)
+        )
 
     return key
